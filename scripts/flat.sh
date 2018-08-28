b47ea44db173a36122c15578e3d0cc66a99954ce
CodeMonkey <philip@codemonkeylab.com>
2018-08-28 13:48:24 +0100

    Added scripts for flatting code

---
+++ scripts/flat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+truffle-flattener contracts/EnergisFees.sol > contracts_flat/EnergisFees.sol
+truffle-flattener contracts/PeriodUtilWeek.sol > contracts_flat/PeriodUtilWeek.sol
---
+++ scripts/genDocs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+SOLC_ARGS='openzeppelin-solidity=~/dev/ZeroCarbonProject/solidityWork/energis-fees/node_modules/openzeppelin-solidity' solidity-docgen ~/dev/ZeroCarbonProject/solidityWork/energis-fees/ ~/dev/ZeroCarbonProject/solidityWork/energis-fees/contracts/ ~/dev/ZeroCarbonProject/solidityWork/energis-fees/docs/
